ec8317b3a9ad7b12041d6365ccf51929dc5b24c6
Ahmet Omak <ahmetaltioglu@hotmail.com>
2022-04-13 22:39:49 +0300

    Update SimpleGrid editor window

--- Assets/Simple Grid/Scripts/Editor/SimpleGridMenu.cs
+++ Assets/Simple Grid/Scripts/Editor/SimpleGridMenu.cs
@@ -1,35 +1,13 @@
 using UnityEditor;
-using UnityEngine;
 
 public static class SimpleGridMenu
 {
-    [MenuItem("Window/Simple Grid/Create Simple Grid Object", false, 200)]
+    [MenuItem("Window/Simple Grid/Create Test Grid Object", false, 200)]
     public static void AddSimpleGridObject()
     {
-        if (Object.FindObjectOfType(typeof(GridManager)) == null)
-        {
-            GameObject go = PrefabUtility.InstantiatePrefab(AssetDatabase.LoadAssetAtPath(WhereIs("GridManager.prefab", "Prefab"), typeof(GameObject))) as GameObject;
-            go.name = "Grid Manager";
-            Selection.activeObject = go;
-            Undo.RegisterCreatedObjectUndo(go, "Created Simple Grid Object");
-        }
-        else
-        {
-            Debug.LogWarning("A Simple Grid object already exists in this scene - you should never have more than one per scene!");
-        }
-    }
-
-    private static string WhereIs(string file, string type)
-    {
-        string[] guids = AssetDatabase.FindAssets("t:" + type);
-        foreach (string guid in guids)
-        {
-            string prefab = AssetDatabase.GUIDToAssetPath(guid);
-            if (prefab.EndsWith(file))
-            {
-                return prefab;
-            }
-        }
-        return "";
+        //TODO: Create an empty Gameobject
+        //TODO: Add a MonoBehaviour with a BaseGridSettings field ready to Create Grid
+        //TODO: Load its BaseGridSettings SO from assets using AssetDataBase
+        //TODO: Hit play and see If it works
     }
 }
